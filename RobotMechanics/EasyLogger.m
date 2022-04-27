42a02ca8e907a95697493757852028307e4dba16
deanocko <dean.ockenden@gmail.com>
2022-04-27 16:14:02 +1000

    Turned off log msgs

--- RobotMechanics/EasyLogger.m
+++ RobotMechanics/EasyLogger.m
@@ -1,5 +1,5 @@
 function EasyLogger(logFile, robot, qCurrent, qTarget)
-%Function that makes it easy for me to log and display transforms
+    %Function that makes it easy for me to log and display transforms
     scriptName = 'in func AnimateRobots';
     transformCurrent = robot.model.fkine(qCurrent);
     transformTarget = robot.model.fkine(qTarget);
@@ -7,8 +7,8 @@ function EasyLogger(logFile, robot, qCurrent, qTarget)
     logFile.mlog = {logFile.DEBUG, scriptName, name};
     logFile.mlog = {logFile.DEBUG, scriptName, ['Current transform is:', logFile.MatrixToString(transformCurrent)]};
     logFile.mlog = {logFile.DEBUG, scriptName, ['Target transform is: ', logFile.MatrixToString(transformTarget)]};
-    disp([newline,name]);
-    disp(strcat('Current transform is: ',logFile.MatrixToString(transformCurrent)));
-    disp(strcat('Target transform is:  ',logFile.MatrixToString(transformTarget)))
+    % todo uncomment for working log msgs
+    %disp([newline,name]);
+    %disp(strcat('Current transform is: ',logFile.MatrixToString(transformCurrent)));
+    %disp(strcat('Target transform is:  ',logFile.MatrixToString(transformTarget)))
 end
-
--- RobotMechanics/MoveFruit.m
+++ RobotMechanics/MoveFruit.m
@@ -10,27 +10,27 @@ function MoveFruit(logFile, robot, fruit, locationFinal)
     fruitFinalPose = transl(locationFinal) * fruitOffset;
 
     %*Go from current pose to fruit and delete it
-    qCurrent = robot.model.getpos();
+    qCurrent = robot.model.getpos()
     qTarget = robot.model.ikcon(fruitPose, qCurrent); %(target pose, current joint angles, mask)
 
     AnimateRobots(logFile, robot, qCurrent, qTarget);
     try delete(fruit); end
 
     %*go from fruit back to intermediary pose
-    qCurrent = robot.model.getpos();
+    qCurrent = robot.model.getpos()
     qTarget = qIntermediary;
 
     AnimateRobots(logFile, robot, qCurrent, qTarget);
 
     %*go from intermediary pose to final fruit pose and place a piece of fruit
-    qCurrent = robot.model.getpos();
+    qCurrent = robot.model.getpos()
     qTarget = robot.model.ikcon(fruitFinalPose, qCurrent); %(target pose, current joint angles, mask)
 
     AnimateRobots(logFile, robot, qCurrent, qTarget);
     Fruit(locationFinal); % todo may need to adjust this so there is a handle for placed fruit
 
     %*go from final fruit position back to intermediary position
-    qCurrent = robot.model.getpos();
+    qCurrent = robot.model.getpos()
     qTarget = qIntermediary;
 
     AnimateRobots(logFile, robot, qCurrent, qTarget);
