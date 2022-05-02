66fded72093acac77ff8ebab56c295fa2588a7c9
deanocko <dean.ockenden@gmail.com>
2022-05-02 17:59:20 +1000

    turned off logs to prevent merge conflicts

--- RobotMechanics/EasyLogger.m
+++ RobotMechanics/EasyLogger.m
@@ -1,15 +1,15 @@
 function EasyLogger(logFile, robot, qCurrent, qTarget)
     %Function that makes it easy for me to log and display transforms
-    scriptName = 'in func AnimateRobots';
-    transformCurrent = robot.model.fkine(qCurrent);
-    transformTarget = robot.model.fkine(qTarget);
-    name = robot.name;
-    logFile.mlog = {logFile.DEBUG, scriptName, name};
-    logFile.mlog = {logFile.DEBUG, scriptName, ['Current transform is:', logFile.MatrixToString(transformCurrent)]};
-    logFile.mlog = {logFile.DEBUG, scriptName, ['Target transform is: ', logFile.MatrixToString(transformTarget)]};
+    %todo all commented out to prevent merge conflicts
+    % scriptName = 'in func AnimateRobots';
+    % transformCurrent = robot.model.fkine(qCurrent);
+    % transformTarget = robot.model.fkine(qTarget);
+    % name = robot.name;
+    % logFile.mlog = {logFile.DEBUG, scriptName, name};
+    % logFile.mlog = {logFile.DEBUG, scriptName, ['Current transform is:', logFile.MatrixToString(transformCurrent)]};
+    % logFile.mlog = {logFile.DEBUG, scriptName, ['Target transform is: ', logFile.MatrixToString(transformTarget)]};
 
-    % todo uncomment for working log msgs
-    %disp([newline,name]);
-    %disp(strcat('Current transform is: ',logFile.MatrixToString(transformCurrent)));
-    %disp(strcat('Target transform is:  ',logFile.MatrixToString(transformTarget)))
+    % disp([newline,name]);
+    % disp(strcat('Current transform is: ',logFile.MatrixToString(transformCurrent)));
+    % disp(strcat('Target transform is:  ',logFile.MatrixToString(transformTarget)))
 end
