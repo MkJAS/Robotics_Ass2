32d6e4044189881d7f7b57ac9013c0dd083a8869
Will-Hoogs <william.hoogervorst@student.uts.edu.au>
2022-05-02 17:17:09 +1000

    Dean looped laser code and I moved to function

--- RobotMechanics/labAssignment2.m
+++ RobotMechanics/labAssignment2.m
@@ -28,18 +28,21 @@ offsetLightCurtain = 0.35;
 PlaceObject('EmergencyButton.ply', [0.5, 0.5, tableHeight]);
 PlaceObject('LightCurtain.ply', [-offsetLightCurtain, offsetLightCurtain, tableHeight]);
 PlaceObject('LightCurtain.ply', [offsetLightCurtain, offsetLightCurtain, tableHeight]);
+
 PlaceObject('LightCurtain.ply', [-offsetLightCurtain, -offsetLightCurtain, tableHeight]);
 PlaceObject('LightCurtain.ply', [offsetLightCurtain, -offsetLightCurtain, tableHeight]);
 
 %% Plot lines between lightcurtains
-offsetVertical = 0.1;
+% offsetVertical = 0.1;
+% 
+% for index = 1:4
+%     startP = ([-offsetLightCurtain, offsetLightCurtain, tableHeight + 0.05 + offsetVertical]);
+%     endP = ([offsetLightCurtain, offsetLightCurtain, tableHeight + 0.05 + offsetVertical]);
+%     plot3([startP(1), endP(1)], [startP(2), endP(2)], [startP(3), endP(3)], 'r');
+%     offsetVertical = offsetVertical + 0.1;
+% end
 
-for index = 1:4
-    startP = ([-offsetLightCurtain, offsetLightCurtain, tableHeight + 0.05 + offsetVertical]);
-    endP = ([offsetLightCurtain, offsetLightCurtain, tableHeight + 0.05 + offsetVertical]);
-    plot3([startP(1), endP(1)], [startP(2), endP(2)], [startP(3), endP(3)], 'r');
-    offsetVertical = offsetVertical + 0.1;
-end
+LightCurtainLasers;
 
 %% Objects
 % PlaceObject('Strawberry.ply', [-0.2, -0.2, tableHeight]);
