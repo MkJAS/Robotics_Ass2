3687fe675f0c6e1e1397dd7c9d1b972a923160cf
Will-Hoogs <william.hoogervorst@student.uts.edu.au>
2022-05-11 15:16:00 +1000

    Add Will path to startup

--- startup.m
+++ startup.m
@@ -3,4 +3,5 @@ addpath(genpath(pwd))
 % addpath(genpath('/Users/Dean/My Drive/University/5th Year/Industrial Robotics/Assessments/A1/Lab Assignment 1/13213537_A1'))
 addpath('/Users/Dean/My Drive/University/5th Year/Industrial Robotics/modifiedRoboticsToolbox')
 addpath('C:\Users\149514\Desktop\rvctools')
+addpath('C:\Users\uncle\Desktop\41013 Industrial Robotics\robot-9.10Small_Modified_20220202_WithVisionTB\robot-9.10Small_Modified_20220202_WithVisionTB\robot-9.10Small_Modified\rvctools')
 startup_rvc
