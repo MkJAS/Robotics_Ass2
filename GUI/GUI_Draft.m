04b63378bdb05ac82efbe1e11ae4d4dc9345c9c9
deanocko <dean.ockenden@gmail.com>
2022-05-16 23:13:06 +1000

    mini push

--- GUI/GUI_Draft.m
+++ GUI/GUI_Draft.m
@@ -658,7 +658,8 @@ function getSelection_Callback(hObject, eventdata, handles)
 
     end
 
-    guidata(hObject, handles);
+    RotateRobot(handles.robot, -30);
+    guidata(hObject, handles); %update object counts
 
 end
 
