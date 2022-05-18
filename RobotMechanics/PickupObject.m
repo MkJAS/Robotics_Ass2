637c94b64bd6f6f183426bc6e645b738fa549e82
deanocko <dean.ockenden@gmail.com>
2022-05-18 16:01:52 +1000

    tweak

--- RobotMechanics/PickupObject.m
+++ RobotMechanics/PickupObject.m
@@ -1,4 +1,4 @@
-function PickupObject(robot, object,points)
+function PickupObject(robot, object, points)
     %Uses RMRC to move robot to the object and then return to it's starting position
     %traj = desired trajectory, lims = robot limits
 
@@ -9,27 +9,25 @@ function PickupObject(robot, object,points)
     endPoint(3) = endPoint(3) + 0.1;
 
     qMatrix = RMRC(midPoint, 1, robot);
-    for i=1:size(qMatrix,1)
-        collision = willCollide(robot, qMatrix(i,:), points);
+
+    for i = 1:size(qMatrix, 1)
+        collision = willCollide(robot, qMatrix(i, :), points);
         intruder = lightCurtainCheck(handles.pcPoints);
+
         if collision == true
             cprintf('red', 'Possible collision detected! Aborting move!\n');
             beep
-            cprintf('red', 'Waiting for obstacle to be removed\n');
-
-
-
+            cprintf('red', 'Waiting for obstacle to be removed\n'); s
         end
 
-
-
-        robot.model.animate(qMatrix(i,:));
+        robot.model.animate(qMatrix(i, :));
         vertices = object.vertices;
-        tr = robot.model.fkine(qMatrix(i,:));
-        transformedVertices = [vertices,ones(size(vertices,1),1)] * tr';
-        set(mesh_h,'Vertices',transformedVertices(:,1:3));
+        tr = robot.model.fkine(qMatrix(i, :));
+        transformedVertices = [vertices, ones(size(vertices, 1), 1)] * tr';
+        set(mesh_h, 'Vertices', transformedVertices(:, 1:3));
         drawnow();
     end
+
     try delete(object); end
     qMatrix = RMRC(endPoint, 1, robot);
 end
